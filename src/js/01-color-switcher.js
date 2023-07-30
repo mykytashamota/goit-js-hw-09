db2c76bc249c5f13db296b171037e00cf33e2a44
mykytashamota <nik.shamota@gmail.com>
2023-07-30 14:54:02 +0300

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+stopBtn.setAttribute('disabled', true);
+startBtn.addEventListener('click', onClickStartBtn);
+function onClickStartBtn() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled', true);
+}
+
+stopBtn.addEventListener('click', onClickStopBtn);
+function onClickStopBtn() {
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled', true);
+}
 
