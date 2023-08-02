e4101811b688f3e5fa20f2e16dbf1ddcc2d84d45
mykytashamota <nik.shamota@gmail.com>
2023-08-02 10:45:42 +0300

    done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onFromSubmitCreatePromises);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFromSubmitCreatePromises(e) {
+  event.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  let dataParams = {};
+  for (const [key, value] of formData.entries()) {
+    dataParams[key] = Number(value);
   }
+  let { amount, step, delay } = dataParams;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay).then(onSuccess).catch(onError);
+    delay += step;
+  }
+  form.reset();
+}
+
+function onSuccess({ position, delay }) {
+  Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+
+function onError({ position, delay }) {
+  Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
